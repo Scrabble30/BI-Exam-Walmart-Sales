7ed3f259cc4b45d82978524653cbce7e97bd6db4
Scrabble30 <144219106+Scrabble30@users.noreply.github.com>
2025-05-26 09:07:55 +0200

    Add a little text to the classification models

--- Streamlit/classification.py
+++ Streamlit/classification.py
@@ -392,6 +392,9 @@ def classification():
                 """)
     elif model_choice == "Random Forest":
         st.subheader("Random Forest Classifier")
+        st.write(
+            "We now train a Random Forest classifier and evaluate its performance using accuracy, classification report, confusion matrix, ROC curve, cross-validation, feature importance, and train/test accuracy."
+        )
 
         randomForestClassifier = load_model('../Data/RandomForestClassifier.pkl')
 
@@ -431,6 +434,9 @@ def classification():
         """)
     else:
         st.subheader("Gaussian Naive Bayes Classifier")
+        st.write(
+            "The last model we train and evaluate is the Gaussian Naive Bayes classifier using accuracy, classification report, confusion matrix, ROC curve, cross-validation, feature importance, and train/test accuracy."
+        )
 
         bayes = load_model('../Data/GaussianNB.pkl')
 
